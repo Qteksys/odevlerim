584b842c61cd73b6efab9a0ac55ef565fc401950
Ömer Tekin <tkn.omerhan@gmail.com>
2021-11-10 12:10:46 +0300

    radiouygulaması

---
+++ radiolaruygulaması/radiolaruygulaması/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace radiolaruygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://canli.radyofresh.com:8000/radyofresh.mp3";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "	http://rdshout.turkuvazgroup.net:8000/";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://n10101m.mediatriple.net/numberone";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "http://46.20.3.201/;type=mp3";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://listen.powerapp.com.tr/powerturk/abr/powerturk/128/playlist.m3u8";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL = "https://radyo.duhnet.tv/dreamturk";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
